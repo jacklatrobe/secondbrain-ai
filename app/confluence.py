d8c2fadf19e1c8912634f08dd1a099fda5af8722
Jack Latrobe <jack@jack-latrobe.com>
2023-05-04 17:32:57 +1000

    Fixed and optimisations

---
+++ app/confluence.py
@@ -0,0 +1,139 @@
+## secondbrain: Second brain and theory of mind experiments with LLMs and LangChain
+## File: secondbrain/confluence.py
+
+import os
+import re
+import base64
+import requests
+import json
+import logging
+from bs4 import BeautifulSoup
+from langchain import OpenAI
+from langchain.agents import Tool
+
+def search_confluence(query) -> str:
+    # Replace these variables with your own values
+    atlassian_email = os.environ.get("CONFLUENCE_CLIENT_ID")
+    api_token = os.environ.get("CONFLUENCE_API_KEY")
+    your_site_name = os.environ.get("CONFLUENCE_SITE_NAME")
+
+    # Encode the email and API token in base64 format
+    credentials = "{atlassian_email}:{api_token}".format(atlassian_email=atlassian_email, api_token=api_token)
+    encoded_credentials = base64.b64encode(bytes(credentials, 'UTF-8')).decode()
+
+    # Set the base URL and the search query parameters
+    base_url = f"https://{your_site_name}.atlassian.net/wiki/rest/api"
+    search_endpoint = "/search"
+    params = {
+        "limit": 2,
+        "cql": "title ~ '{query}' or text ~ '{query}'".format(query=query),
+        #"space": "SNB",
+    }
+
+    # Set the headers, including the encoded credentials for authorization
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic {encoded_credentials}".format(encoded_credentials=encoded_credentials)
+    }
+
+    # Make the GET request to the Confluence API
+    response = requests.get(base_url + search_endpoint, headers=headers, params=params)
+
+    # Check if the request was successful (HTTP status code 200) and build YAML response obj
+    if response.status_code == 200:
+        results = response.json()
+        return_str = "Search Results for '{query}':\n".format(query=query)
+        res_counter = 0
+        for result in results["results"]:
+            if "content" in result:
+                if result["content"]["type"] == "page":
+                    res_counter += 1
+                    title = result["content"]["title"]
+                    page_url = result["content"]["_links"]["self"]
+                    params = {"expand": "body.storage"}
+                    page_request = requests.get(page_url, headers=headers, params=params)
+                    body_html = page_request.json()["body"]["storage"]["value"]
+                    re_obj = re.compile(r'<ac.*?/>')
+                    body_html = re_obj.sub("", body_html)
+                    body_text = BeautifulSoup(body_html, "html.parser")
+                    body_text = "\n".join(body_text.text.split("\n"))
+                    summary_llm = OpenAI(max_tokens=750)
+                    body_text = summary_llm("Re-write the following text into a single large paragraph, retaining as much information as possible: {body_text}".format(body_text=body_text))
+                    result_str = " - title: {title}\n   body_text: '{body_text}'\n".format(title=title, body_text=body_text)
+                    return_str = "{}{}".format(return_str, result_str)
+        if res_counter > 0:
+            return return_str
+        else:
+            logging.info("No confluence results for: {query}".format(query=query))
+            return "No search results found - use another tool to find an answer for the user"
+    else:
+        logging.warn("Confluence search request failed with status code {response_code}".format(response_code=response.status_code))
+        return "Request failed with status code {response_code}".format(response_code=response.status_code)
+    
+
+def save_to_confluence(text) -> str:
+    # Replace these variables with your own values
+    atlassian_email = os.environ.get("CONFLUENCE_CLIENT_ID")
+    api_token = os.environ.get("CONFLUENCE_API_KEY")
+    your_site_name = os.environ.get("CONFLUENCE_SITE_NAME")
+
+    # Encode the email and API token in base64 format
+    credentials = "{atlassian_email}:{api_token}".format(atlassian_email=atlassian_email, api_token=api_token)
+    encoded_credentials = base64.b64encode(bytes(credentials, 'UTF-8')).decode()
+
+    # Create a title
+    summary_llm = OpenAI(max_tokens=128)
+    title = summary_llm("What is a good title for a page containing this text: {text}".format(text=text))
+
+    expansion_llm = OpenAI(max_tokens=2000)
+    if len(text) < 1000:
+        text = expansion_llm("Re-write and expand this text into a full page article, creating additional headings with information where necessary, and format it as body HTML for a confluence page: {text}".format(text=text))
+    else:
+        text = expansion_llm("Format this text as HTML, with headings, lists and paragraphs: {text}".format(text=text))
+
+    # Set the base URL and the search query parameters
+    base_url = f"https://{your_site_name}.atlassian.net/wiki/api"
+    create_endpoint = "/v2/pages"
+    
+    # Set the headers, including the encoded credentials for authorization
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Basic {encoded_credentials}".format(encoded_credentials=encoded_credentials)
+    }
+
+    # Make the POST request to the Confluence API
+    payload = json.dumps( {
+        "spaceId": 26378243,
+        "status": "current",
+        "title": title,
+        "body": {
+            "representation": "storage",
+            "value": text
+        }
+        } )
+    response = requests.request(
+        "POST",
+        base_url + create_endpoint,
+        data=payload,
+        headers=headers
+    )
+
+    # Check if the request was successful (HTTP status code 200) and build YAML response obj
+    if response.status_code == 200:
+        return "Article was successfully created: {title}".format(title=title)
+    else:
+        print(response.text)
+        return "Unable to save article. Request failed with status code {response_code}".format(response_code=response.status_code)
+    
+SearchConfluenceTool = Tool(
+    name = "Search the knowledgebase",
+    func = search_confluence,
+    description="Use this tool first. Useful for searching the users private confluence knowledgebase for information, articles or documents. Input should be key words to search for."
+)
+
+SaveConfluenceTool = Tool(
+    name = "Save knowledgebase article",
+    func = save_to_confluence,
+    description="Useful for saving new pages to the users private confluence knowledgebase. Input must be a full page of text"
+)
--- app/main.py
+++ app/main.py
@@ -1,40 +1,39 @@
 ## secondbrain: Second brain and theory of mind experiments with LLMs and LangChain
 ## File: secondbrain/main.py
 
+import os
+os.environ["LANGCHAIN_HANDLER"] = "langchain"
+
 from langchain import OpenAI, LLMMathChain
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
+from langchain import PromptTemplate
+from langchain.chains.conversation.memory import ConversationSummaryMemory
 from langchain.agents import Tool
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.utilities import WikipediaAPIWrapper
 from langchain.utilities import OpenWeatherMapAPIWrapper
 from langchain.utilities import GoogleSerperAPIWrapper
-from bs4 import BeautifulSoup
 import human_prompts
-import os
-import re
-import base64
-import requests
-
-os.environ["LANGCHAIN_HANDLER"] = "langchain"
+import secondbrain_agent
+import confluence
 
 def main() -> None:
     wikipedia = WikipediaAPIWrapper(top_k_results=2)
     googler = GoogleSerperAPIWrapper()
     weather = OpenWeatherMapAPIWrapper()
-    llm = OpenAI(temperature=0)
-    math_llm = LLMMathChain(llm=llm)
+    llm = OpenAI(temperature=0.1, max_tokens=1000)
+    math_llm = LLMMathChain.from_llm(llm)
     tools = [
         Tool(
             name="Web Search",
             func=googler.run,
-            description="Useful for searching Google to look up answers to questions, or information about other websites. Input should be a search query."
+            description="Useful for searching Google to look up information or answers to questions, or information about other websites. Input should be a search query."
         ),
         Tool(
             name="Encyclopedia Search",
             func=wikipedia.run,
-            description="Useful for searching an encyclopedia for information about historical events, places or famous people. Input should be a simple search query."
+            description="Useful for searching an encyclopedia for information about concepts, historical events, places or famous people. Input should be key words to search for."
         ),
         Tool(
             name="Weather Search",
@@ -46,21 +45,15 @@ def main() -> None:
             func=math_llm.run,
             description="Useful for when you need to answer questions about math."
         ),
-        Tool(
-            name="Ask a human",
-            func=human_prompts.handle_query,
-            description="As a last resort, this tool allows you to ask the human a specific question when additional context is required to answer a query"
-        ),
-        Tool(
-            name="Search the knowledgebase",
-            func=search_confluence,
-            description="Always use this tool. Searches the users private knowledgebase for information, articles or documents. Input should be a very short search query."
-        )
+        confluence.SearchConfluenceTool,
+        confluence.SaveConfluenceTool,
         ## Additional tools here. Eg: Search Salesforce, Query JIRA, Interact with Pandas etc. etc.
     ]
 
     chat_llm = ChatOpenAI(temperature=0.1, max_tokens=500, verbose=True)
-    chat_memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conv_summary_llm = OpenAI(max_tokens=500)
+    chat_memory = ConversationSummaryMemory(llm=conv_summary_llm, memory_key="chat_history", return_messages=True)
+
     agent_chain = initialize_agent(tools, chat_llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, memory=chat_memory)
 
     continue_conversation = True
@@ -70,58 +63,7 @@ def main() -> None:
         except Exception as ex:
             print("Conversation ended: {}".format(ex))
 
-def search_confluence(query) -> str:
-    # Replace these variables with your own values
-    atlassian_email = os.environ.get("CONFLUENCE_CLIENT_ID")
-    api_token = os.environ.get("CONFLUENCE_API_KEY")
-    your_site_name = os.environ.get("CONFLUENCE_SITE_NAME")
 
-    # Encode the email and API token in base64 format
-    credentials = "{atlassian_email}:{api_token}".format(atlassian_email=atlassian_email, api_token=api_token)
-    encoded_credentials = base64.b64encode(bytes(credentials, 'UTF-8')).decode()
-
-    # Set the base URL and the search query parameters
-    base_url = f"https://{your_site_name}.atlassian.net/wiki/rest/api"
-    search_endpoint = "/search"
-    params = {
-        "limit": 2,
-        "cql": "title ~ '{query}' or text ~ '{query}'".format(query=query),
-        #"space": "SNB",
-    }
-
-    # Set the headers, including the encoded credentials for authorization
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Basic {encoded_credentials}".format(encoded_credentials=encoded_credentials)
-    }
-
-    # Make the GET request to the Confluence API
-    response = requests.get(base_url + search_endpoint, headers=headers, params=params)
-
-    # Check if the request was successful (HTTP status code 200) and build YAML response obj
-    if response.status_code == 200:
-        results = response.json()
-        return_str = "Search Results for '{query}':\n".format(query=query)
-        for result in results["results"]:
-            if "content" in result:
-                if result["content"]["type"] == "page":
-                    title = result["content"]["title"]
-                    page_url = result["content"]["_links"]["self"]
-                    params = {"expand": "body.storage"}
-                    page_request = requests.get(page_url, headers=headers, params=params)
-                    body_html = page_request.json()["body"]["storage"]["value"]
-                    re_obj = re.compile(r'<ac.*?/>')
-                    body_html = re_obj.sub("", body_html)
-                    body_text = BeautifulSoup(body_html, "html.parser")
-                    body_text = "\n".join(body_text.text.split("\n"))
-                    summary_llm = OpenAI(max_tokens=500)
-                    body_text = summary_llm("Succinctly summarise this text for inclusion in a search result about {query}: {body_text}".format(query=query, body_text=body_text))
-                    result_str = " - title: {title}\n   body_text: '{body_text}'\n".format(title=title, body_text=body_text)
-                    return_str = "{}{}".format(return_str, result_str)
-        print(return_str)
-        return return_str
-    else:
-        return "Request failed with status code {response_code}".format(response_code=response.status_code)
 
 
 if __name__ == "__main__":
